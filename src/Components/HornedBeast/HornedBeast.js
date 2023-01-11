d6ec1f1e19a42ac6b9493179d4a5ba320301faac
Marco A. Villafana <pastry07gmail.com>
2023-01-11 19:20:33 +0000

    updated css for cards and added it to HornedBeast.css

--- src/Components/HornedBeast/HornedBeast.js
+++ src/Components/HornedBeast/HornedBeast.js
@@ -1,6 +1,8 @@
 // import { toHaveDisplayValue } from '@testing-library/jest-dom/dist/matchers';
 import React from 'react';
 import Card from 'react-bootstrap/Card';
+// import Col from 'react-bootstrap/Col';
+// import Row from 'react-bootstrap/Row';
 import './HornedBeast.css';
 
 
@@ -31,8 +33,23 @@ class HornedBeast extends React.Component
         <p>{this.state.favorites} ⭐️</p>
         <img onClick={this.handleFavs} src={this.props.image_url} alt={this.props.description} title={this.props.title}/>
         <p>{this.props.description}</p> */}
-      
-        <Card style={{ width: '18rem', marginTop: '1rem', backgroundColor: '#323030', color: '#FFF'}} >
+
+          {/* <Row xs={1} md={2} className="g-4">
+                {Array.from({ length: 1 }).map((_, idx) => (
+                  <Col>
+                    <Card style={{ width: '18rem', marginTop: '1rem', backgroundColor: '#323030', color: '#FFF'}}>
+                      <Card.Img variant="top" style={{height: '260px', }} onClick={this.handleFavs} src={this.props.image_url} alt={this.props.description} title={this.props.title}/>
+                      <Card.Body>
+                        <Card.Title>{this.props.title} ⭐️{this.state.favorites}</Card.Title>
+                        <Card.Text>{this.props.description}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+              </Row> */}
+
+{/* style={{ width: '18rem', marginTop: '1rem', backgroundColor: '#323030', color: '#FFF'}} */}
+        <Card >
           <Card.Img style={{height: '260px'}} variant="top" onClick={this.handleFavs} src={this.props.image_url} alt={this.props.description} title={this.props.title} />
           <Card.Body>
             <Card.Title>{this.props.title} ⭐️{this.state.favorites}</Card.Title>
